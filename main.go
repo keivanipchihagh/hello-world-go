2d35bd8f0d391f3e4a57fffa0e6f3f9279f33e1d
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2025-03-22 10:22:17 +0330

    feat: Add Web Server and Dot Env

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func helloWorld(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World")
+}
+
+func helloWorldName(c *gin.Context) {
+	name := c.Param("name")
+	msg := fmt.Sprintf("Hello World, %v!", name)
+	c.String(http.StatusOK, msg)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// Load .env files
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not find .env file")
+	}
+
+	router := gin.Default()
+	router.GET("hello-world", helloWorld)
+	router.GET("hello-world/:name", helloWorldName)
+
+	host := os.Getenv("API_HOST")
+	port := os.Getenv("API_PORT")
+
+	address := fmt.Sprintf("%v:%v", host, port)
+	router.Run(address)
 }
