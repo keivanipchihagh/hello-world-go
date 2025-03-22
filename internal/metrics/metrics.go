0c45dbff3eb8473d1a101f8350e38d98c9b6ad72
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2025-03-22 21:07:43 +0330

    feat: Add prometheus metrics

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,33 @@
+package metrics
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "http_request_duration_seconds",
+		Help:    "Duration of HTTP requests in seconds",
+		Buckets: prometheus.DefBuckets,
+	}, []string{"path", "method", "status"})
+)
+
+func init() {
+	prometheus.MustRegister(httpDuration)
+}
+
+func PrometheusMetrics() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		// Process request
+		c.Next()
+
+		duration := time.Since(start).Seconds()
+		status := c.Writer.Status()
+		httpDuration.WithLabelValues(c.FullPath(), c.Request.Method, string(status)).Observe(duration)
+	}
+}
--- internal/service.go
+++ internal/service.go
@@ -3,16 +3,24 @@ package internal
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/keivanipchihagh/hello-world-go/api/http"
+	"github.com/keivanipchihagh/hello-world-go/internal/metrics"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Start() {
 	router := gin.Default()
 
+	router.Use(metrics.PrometheusMetrics())
+
+	// Register your API routes
 	router.GET("tasks", http.GetTasks)
 	router.GET("tasks/:id", http.GetTask)
 	router.POST("tasks", http.AddTask)
 	router.DELETE("tasks/:id", http.DeleteTask)
 	router.PUT("tasks/:id", http.UpdateTask)
+
+	// Expose Prometheus metrics on /metrics
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	router.Run()
 }
